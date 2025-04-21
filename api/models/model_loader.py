4f37f277bf6b832cc16fc7a8ef4ae438f291a229
Magnetize8953 <aha6@charlotte.edu>
2025-04-21 14:43:05 -0400

    chore!: remove unnecessary skeleton code
    BREAKING CHANGE: multiple models, controllers, routes removed

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,18 +1,10 @@
-# TODO: remove unnecessary imports
-from . import orders, order_details, recipes, sandwiches, resources
-from . import customers, payments, reviews, menu, promotions
+from . import orders, resources, customers, payments, reviews, menu, promotions
 from ..dependencies.database import engine
 
 
 def index():
-    # given in skeleton
-    # TODO: remove unnecessary creations
     orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
-
     customers.Base.metadata.create_all(engine)
     payments.Base.metadata.create_all(engine)
     reviews.Base.metadata.create_all(engine)

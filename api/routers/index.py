c3aeeb3d24f07e640723b3ca2cc943eb9423d1b6
Chloe_Siharath <Csiharat@Charlotte.edu>
2025-04-23 15:44:24 -0400

    chore: update db name

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,7 +1,8 @@
-from . import orders, menu, customers
+from . import orders, menu, customers, resources
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(menu.router)
     app.include_router(customers.router)
+    app.include_router(resources.router)
